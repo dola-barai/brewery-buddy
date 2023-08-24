bb0f4ba8824a244a893fba5c5cae03c1c48a9581
Dola Barai <dola43dc@gmail.com>
2023-08-24 16:05:06 +0600

    login added

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -24,13 +24,13 @@ const Login = () => {
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" placeholder="email" className="input input-bordered" required/>
+                                <input type="email" placeholder="email" className="input input-bordered" required/>
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="text" placeholder="password" className="input input-bordered" required/>
+                                <input type="password" placeholder="password" className="input input-bordered" required/>
                                 
                             </div>
                             <div className="form-control mt-6">
